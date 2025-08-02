82e792235d789b6685ac3f6662e9ea6543d9a56a
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-08-02 11:13:08 +0530

    initial commit

---
+++ src/main/java/com/itp/breathsafe/BreatheSafeServerApplication.java
@@ -0,0 +1,13 @@
+package com.itp.breathsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreatheSafeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreatheSafeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itp/breathsafe/BreatheSafeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itp.breathsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreatheSafeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
