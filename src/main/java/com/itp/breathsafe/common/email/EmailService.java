b91a63db6dee0e7833945b54ec348b29aae0c601
Anupa Prabhasara <info@anupa.lk>
2025-09-22 21:40:52 +0530

    email-service

--- src/main/java/com/itp/breathsafe/common/email/EmailService.java
+++ src/main/java/com/itp/breathsafe/common/email/EmailService.java
@@ -1,4 +1,29 @@
 package com.itp.breathsafe.common.email;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+@Service
 public class EmailService {
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        mailSender.send(message);
+    }
 }
