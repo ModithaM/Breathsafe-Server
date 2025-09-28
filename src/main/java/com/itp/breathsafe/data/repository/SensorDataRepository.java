7bba00ce785fc426aba83abd181753ab897e27ef
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-09-28 10:00:04 +0530

    Remove unwanted comment lines

--- src/main/java/com/itp/breathsafe/data/repository/SensorDataRepository.java
+++ src/main/java/com/itp/breathsafe/data/repository/SensorDataRepository.java
@@ -56,7 +56,4 @@ public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
             @Param("aqiCategory") AQICategory aqiCategory,
             @Param("co2Level") Integer co2Level
     );
-
-    // Helper method to find by ID
-//    Optional<SensorData> findByIdForUpdate(Long dataId);
 }
